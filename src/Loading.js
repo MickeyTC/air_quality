0b04408bc722edc17e9c6e1022b86bac61d561b8
Teerapat Chintanakit <t.chintanakit@gmail.com>
2020-12-21 13:09:24 +0700

    Use Styled-components in Loading

---
+++ src/Loading.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import styled, { keyframes } from 'styled-components'
+import { loading } from './assets'
+
+const Wrapper = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: #00000066;
+  z-index: 100;
+`
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+const Img = styled.img`
+  height: 40vmin;
+  width: 40vmin;
+  pointer-events: none;
+  animation: ${rotate} infinite 5s linear;
+`
+
+const Loading = props => {
+  return (
+    <Wrapper>
+      <Img src={loading} alt='loading' />
+    </Wrapper>
+  )
+}
+
+export default Loading
