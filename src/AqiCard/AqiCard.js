0fa59fc6c11660d22c579ce65ab9c723697f9336
Teerapat Chintanakit <t.chintanakit@gmail.com>
2020-12-21 10:04:34 +0700

    Style for all conditions

--- src/AqiCard/AqiCard.js
+++ src/AqiCard/AqiCard.js
@@ -1,7 +1,56 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './AqiCard.css'
-import { faceGreen, humidity, windDirection } from '../assets'
+import {
+  faceGreen,
+  faceMaroon,
+  faceOrange,
+  facePurple,
+  faceRed,
+  faceYellow,
+  humidity,
+  windDirection,
+} from '../assets'
+
+const getAqiStyle = aqi => {
+  if (aqi <= 50) {
+    return {
+      face: faceGreen,
+      condition: 'Good',
+      className: 'aqi-green',
+    }
+  } else if (aqi <= 100) {
+    return {
+      face: faceYellow,
+      condition: 'Moderate',
+      className: 'aqi-yellow',
+    }
+  } else if (aqi <= 150) {
+    return {
+      face: faceOrange,
+      condition: 'Unhealthy for sensitive groups',
+      className: 'aqi-orange',
+    }
+  } else if (aqi <= 200) {
+    return {
+      face: faceRed,
+      condition: 'Unhealthy',
+      className: 'aqi-red',
+    }
+  } else if (aqi <= 300) {
+    return {
+      face: facePurple,
+      condition: 'Very unhealthy',
+      className: 'aqi-purple',
+    }
+  } else {
+    return {
+      face: faceMaroon,
+      condition: 'Hazardous',
+      className: 'aqi-maroon',
+    }
+  }
+}
 
 const AqiCard = props => {
   const {
@@ -11,24 +60,28 @@ const AqiCard = props => {
       country = '',
       current: {
         weather: { tp = 30, hu = 60, ws = 10, wd = 90, ic = '01d' } = {},
-        pollution: { aqius } = {},
+        pollution: { aqius = 0, aqicn = 0 } = {},
       } = {},
     } = {},
+    useUSAqi = true,
   } = props
+
+  const aqi = useUSAqi ? aqius : aqicn
+  const { face, condition, className: aqiClass } = getAqiStyle(aqi)
 
   return (
     <div className='container'>
       <div className='location box column'>
-        <div className='city'>{city}</div>
-        <div className='state'>{`${state}, ${country}`}</div>
+        <div className='city info'>{city}</div>
+        <div className='state info'>{`${state}, ${country}`}</div>
       </div>
-      <div className='pollution box'>
-        <img className='face-aqi info' src={faceGreen} alt={''} />
+      <div className={`pollution box ${aqiClass}`}>
+        <img className='face-aqi info' src={face} alt={''} />
         <div className='aqi info column'>
-          <div className='aqi-value'>{aqius}</div>
-          <div className='aqi-title'>{'US AQI'}</div>
+          <div className='aqi-value'>{aqi}</div>
+          <div className='aqi-title'>{useUSAqi ? 'US AQI' : 'CN AQI'}</div>
         </div>
-        <div className='condition info'>{'Good'}</div>
+        <div className='condition info'>{condition}</div>
       </div>
       <div className='weather box'>
         <div className='temperature info'>
@@ -37,7 +90,7 @@ const AqiCard = props => {
             src={`https://www.airvisual.com/images/${ic}.png`}
             alt={`icon-${ic}`}
           />
-          <div className='temperature'>{`${tp}°C`}</div>
+          <div className='temperature-value'>{`${tp}°C`}</div>
         </div>
         <div className='wind info'>
           <img
@@ -72,9 +125,11 @@ AqiCard.propTypes = {
       }),
       pollution: PropTypes.shape({
         aqius: PropTypes.number,
+        aqicn: PropTypes.number,
       }),
     }),
   }),
+  useUSAqi: PropTypes.bool,
 }
 
 export default AqiCard
