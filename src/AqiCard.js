0c733104e99d1e9550af2aed8e2b7e428c5c6d46
Teerapat Chintanakit <t.chintanakit@gmail.com>
2020-12-23 21:33:14 +0700

    Use weather from air visual

--- src/AqiCard.js
+++ src/AqiCard.js
@@ -8,7 +8,6 @@ import {
   facePurple,
   faceRed,
   faceYellow,
-  getWeatherIcon,
   humidity,
   windDirection,
 } from './assets'
@@ -184,7 +183,12 @@ const AqiCard = props => {
       </PollutionBox>
       <WeatherBox>
         <Info>
-          {!!ic && <WeatherIcon src={getWeatherIcon(ic)} alt={`icon-${ic}`} />}
+          {!!ic && (
+            <WeatherIcon
+              src={`https://www.airvisual.com/images/${ic}.png`}
+              alt={`icon-${ic}`}
+            />
+          )}
           {tp !== undefined && <WeatherValue>{`${tp}°C`}</WeatherValue>}
         </Info>
         <Info>
