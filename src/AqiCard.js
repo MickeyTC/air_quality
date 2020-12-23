d4edc80fb93e48a2410f02eb205cf898d96df9d3
Teerapat Chintanakit <t.chintanakit@gmail.com>
2020-12-23 21:47:41 +0700

    Revert "Use weather from air visual"
    This reverts commit 0c733104e99d1e9550af2aed8e2b7e428c5c6d46.

--- src/AqiCard.js
+++ src/AqiCard.js
@@ -8,6 +8,7 @@ import {
   facePurple,
   faceRed,
   faceYellow,
+  getWeatherIcon,
   humidity,
   windDirection,
 } from './assets'
@@ -183,12 +184,7 @@ const AqiCard = props => {
       </PollutionBox>
       <WeatherBox>
         <Info>
-          {!!ic && (
-            <WeatherIcon
-              src={`https://www.airvisual.com/images/${ic}.png`}
-              alt={`icon-${ic}`}
-            />
-          )}
+          {!!ic && <WeatherIcon src={getWeatherIcon(ic)} alt={`icon-${ic}`} />}
           {tp !== undefined && <WeatherValue>{`${tp}°C`}</WeatherValue>}
         </Info>
         <Info>
--- src/assets/index.js
+++ src/assets/index.js
@@ -7,8 +7,51 @@ import faceOrange from './ic-face-orange.svg'
 import faceRed from './ic-face-red.svg'
 import facePurple from './ic-face-purple.svg'
 import faceMaroon from './ic-face-maroon.svg'
+import weather01d from './01d.png'
+import weather01n from './01n.png'
+import weather02d from './02d.png'
+import weather02n from './02n.png'
+import weather03d from './03d.png'
+import weather04d from './04d.png'
+import weather09d from './09d.png'
+import weather10d from './10d.png'
+import weather10n from './10n.png'
+import weather11d from './11d.png'
+import weather13d from './13d.png'
+import weather50d from './50d.png'
 import addIcon from './add.svg'
 import deleteIcon from './delete.svg'
+
+const getWeatherIcon = icon => {
+  switch (icon) {
+    case '01d':
+      return weather01d
+    case '01n':
+      return weather01n
+    case '02d':
+      return weather02d
+    case '02n':
+      return weather02n
+    case '03d':
+      return weather03d
+    case '04d':
+      return weather04d
+    case '09d':
+      return weather09d
+    case '10d':
+      return weather10d
+    case '10n':
+      return weather10n
+    case '11d':
+      return weather11d
+    case '13d':
+      return weather13d
+    case '50d':
+      return weather50d
+    default:
+      return
+  }
+}
 
 export {
   loading,
@@ -20,6 +63,7 @@ export {
   faceRed,
   facePurple,
   faceMaroon,
+  getWeatherIcon,
   addIcon,
   deleteIcon,
 }
